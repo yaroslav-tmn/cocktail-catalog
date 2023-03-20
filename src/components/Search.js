eb867e345ac0baa06ce628e662f01aad9d56a23c
Yaroslav Ostapenko <yaroslav.ostapenko@gmail.com>
2023-03-20 18:30:45 +0500

    adaptivity

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,20 @@
 import { Col, Row, FormSelect } from 'react-bootstrap';
 
+//<Col xxl='auto' xl='auto' lg='auto' md='auto' xs sm>
+
 export default function Search() {
   return (
-    <Row className='mx-5 p-3 row justify-content-start border border-1 rounded-3 align-items-center'>
-      <Col className='col-1'>Search by:</Col>
-      <Col className='col-2'>
+    <Row className='mx-5 p-3 justify-content-start border border-1 rounded-3 align-items-center d-flex flex-row flex-wrap'>
+      <Col xs='auto' className='p-1'>
+        Search by:
+      </Col>
+      <Col md='auto' sm xs className='p-1'>
         <FormSelect>
           <option value='1'>name</option>
           <option value='2'>ingredient</option>
         </FormSelect>
       </Col>
-      <Col>
+      <Col xxl md className='p-1'>
         <input className='form-control'></input>
       </Col>
     </Row>
